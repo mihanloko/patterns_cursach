f5215d74f19a6942afdd46786712a57b4e0c25cc
Mikhail <rusmih98@yandex.ru>
2020-01-17 21:03:37 +0700

    thymeleaf

--- src/main/java/patterns/cursach/controller/CensorController.java
+++ src/main/java/patterns/cursach/controller/CensorController.java
@@ -1,16 +1,17 @@
 package patterns.cursach.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import patterns.cursach.Censor;
 
-@RestController
+@Controller
 public class CensorController {
 
     @RequestMapping(value="/test", method= RequestMethod.GET)
     public String test() {
-        return "test";
+        return "build/index";
     }
 
 }
