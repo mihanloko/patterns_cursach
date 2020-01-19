ff7b4f1c63086028ff37b41b330923a6769acf71
Mikhail <rusmih98@yandex.ru>
2020-01-19 14:51:33 +0700

    осталось связать этот хлам

--- src/main/java/patterns/cursach/controller/CensorController.java
+++ src/main/java/patterns/cursach/controller/CensorController.java
@@ -1,17 +1,31 @@
 package patterns.cursach.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import patterns.cursach.Censor;
 
 @Controller
 public class CensorController {
 
-    @RequestMapping(value="/test", method= RequestMethod.GET)
-    public String test() {
-        return "build/index";
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String page(@RequestParam(name = "text", required = false, defaultValue = "") String text, Model model) {
+        model.addAttribute("text", text);
+        return "index";
     }
 
+    @RequestMapping(value = "/handle", method = RequestMethod.POST)
+    public String handle(
+            @RequestParam(name = "source", required = false, defaultValue = "") String text,
+            @RequestParam(name = "algorithm", required = false, defaultValue = "0") int algorithm,
+            @RequestParam(name = "strategy", required = false, defaultValue = "0") int strategy,
+            RedirectAttributes attributes) {
+
+        attributes.addAttribute("text", "hello there");
+        return "redirect:test";
+    }
 }
